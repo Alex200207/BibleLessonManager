08d1ac3a2cf2fe076cd6baae556777fe67c9a203
soy eddy <soyeddy.email.com>
2024-10-26 23:54:25 -0600

    agregando un footer

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,7 +2,7 @@ import AuthLayout from "../../src/layout/AuthLayout";
 import { useLoginValidation } from "../hooks/useLoginValidation";
 import { Header } from "../components/Layout/login/Header";
 import { Form } from "../components/Layout/login/Form";
-import AlexApp from '../assets/image/@AlexApp.png'
+import AlexApp from "../assets/image/@AlexApp.png";
 
 const Login = () => {
   const {
@@ -21,29 +21,36 @@ const Login = () => {
       <>
         <Header />
         <div
-          className="flex items-center justify-center h-screen"
+          className="flex flex-col h-screen "
           style={{
             backgroundImage: `url(${AlexApp})`,
             backgroundSize: "cover",
             backgroundPosition: "center",
           }}
         >
-          <div className="w-full max-w-md p-5 space-y-6 bg-white shadow-md rounded-lg mx-1">
-            <h4 className="text-2xl font-bold text-center text-gray-700">
-              Bienvenido a BibleLesson
-            </h4>
-            <p className="text-center text-gray-500">Ingrese tus datos</p>
-            <Form
-              onSubmit={handleSubmit}
-              setEmail={setEmail}
-              setPassword={setPassword}
-              validateEmail={validateEmail}
-              validatePassword={validatePassword}
-              email={email}
-              password={password}
-              loading={loading}
-            />
+          <div className="flex-grow flex items-center justify-center">
+            <div className="w-full max-w-md p-4 space-y-4 bg-white shadow-md rounded-lg mx-2 mt-16 md:mt-8">
+              <h4 className="text-2xl font-bold text-center text-gray-700">
+                Bienvenido a BibleLesson
+              </h4>
+              <p className="text-center text-gray-500">Ingrese sus datos</p>
+              <Form
+                onSubmit={handleSubmit}
+                setEmail={setEmail}
+                setPassword={setPassword}
+                validateEmail={validateEmail}
+                validatePassword={validatePassword}
+                email={email}
+                password={password}
+                loading={loading}
+              />
+            </div>
           </div>
+          <footer className=" shadow-md py-2 w-full p-4">
+            <p className="text-center text-gray-500">
+              &copy; 2024 BibleLesson. Todos los derechos reservados.
+            </p>
+          </footer>
         </div>
       </>
     </AuthLayout>
