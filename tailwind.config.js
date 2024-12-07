382e71f3266fcdbe9c45e353ed88ca10d98b2b15
soy eddy <soyeddy.email.com>
2024-12-07 13:56:29 -0600

    remove

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  darkMode: "class", // O 'media' si prefieres que se base en la configuración del sistema
+  darkMode: "class", 
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
-        "dark-bg": "#2d3748", // Puedes personalizar los colores del fondo
-        "dark-text": "#e2e8f0", // Y los colores de texto para modo oscuro
+        "dark-bg": "#2d3748", 
+        "dark-text": "#e2e8f0", 
       },
     },
   },
