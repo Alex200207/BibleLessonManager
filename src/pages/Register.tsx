67dbc73197ce9291fd181911d66f85fd2f5e28c9
soy eddy <soyeddy.email.com>
2024-12-12 21:18:14 -0600

    fix

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -2,7 +2,6 @@ import AuthLayout from "../../src/layout/AuthLayout";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
-import AlexApp from '../assets/image/@AlexApp.png'
 
 const Register = () => {
   const navigate = useNavigate();
@@ -17,48 +16,48 @@ const Register = () => {
 
     if (!name) {
       Swal.fire({
-        icon: 'error',
-        title: 'Error',
-        text: 'El nombre es requerido',
+        icon: "error",
+        title: "Error",
+        text: "El nombre es requerido",
       });
       isValid = false;
     }
     if (!email) {
       Swal.fire({
-        icon: 'error',
-        title: 'Error',
-        text: 'El correo es requerido',
+        icon: "error",
+        title: "Error",
+        text: "El correo es requerido",
       });
       isValid = false;
     } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
       Swal.fire({
-        icon: 'error',
-        title: 'Error',
-        text: 'Formato de correo erróneo',
+        icon: "error",
+        title: "Error",
+        text: "Formato de correo erróneo",
       });
       isValid = false;
     }
 
     if (!password) {
       Swal.fire({
-        icon: 'error',
-        title: 'Error',
-        text: 'La contraseña es requerida',
+        icon: "error",
+        title: "Error",
+        text: "La contraseña es requerida",
       });
       isValid = false;
     }
     if (!confirmPassword) {
       Swal.fire({
-        icon: 'error',
-        title: 'Error',
-        text: 'La confirmación de la contraseña es requerida',
+        icon: "error",
+        title: "Error",
+        text: "La confirmación de la contraseña es requerida",
       });
       isValid = false;
     } else if (password !== confirmPassword) {
       Swal.fire({
-        icon: 'error',
-        title: 'Error',
-        text: 'Las contraseñas no coinciden',
+        icon: "error",
+        title: "Error",
+        text: "Las contraseñas no coinciden",
       });
       isValid = false;
     }
@@ -81,9 +80,9 @@ const Register = () => {
 
         if (response.ok) {
           Swal.fire({
-            icon: 'success',
-            title: 'Registrado',
-            text: 'Registro exitoso. Redirigiendo al inicio de sesión...',
+            icon: "success",
+            title: "Registrado",
+            text: "Registro exitoso. Redirigiendo al inicio de sesión...",
             showConfirmButton: false,
             timer: 2000,
           }).then(() => {
@@ -92,16 +91,16 @@ const Register = () => {
         } else {
           const responseData = await response.json();
           Swal.fire({
-            icon: 'error',
-            title: 'Error',
-            text: responseData.message || 'Error al registrar el usuario',
+            icon: "error",
+            title: "Error",
+            text: responseData.message || "Error al registrar el usuario",
           });
         }
       } catch {
         Swal.fire({
-          icon: 'error',
-          title: 'Error',
-          text: 'Error en la conexión con el servidor',
+          icon: "error",
+          title: "Error",
+          text: "Error en la conexión con el servidor",
         });
       }
     }
@@ -110,17 +109,18 @@ const Register = () => {
   return (
     <AuthLayout>
       <div
-          className="flex items-center justify-center h-screen"
-          style={{
-            backgroundImage: `url(${AlexApp})`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            
-          }}
-        >
+        className="flex items-center justify-center h-screen"
+        style={{
+          background: "linear-gradient(to bottom, #0a0f40, #2569a9)",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
         <div className="bg-white shadow-md rounded-lg overflow-hidden max-w-md w-full">
           <div className="p-6">
-            <h4 className="text-2xl font-bold text-center">Registrarse en AlexStore</h4>
+            <h4 className="text-2xl font-bold text-center">
+              Registrarse en AlexStore
+            </h4>
             <p className="text-center text-gray-600 mb-4">Ingrese tus datos</p>
 
             <form onSubmit={handleSubmit} className="space-y-4">
@@ -163,13 +163,19 @@ const Register = () => {
                 />
               </div>
               <div className="flex justify-between items-center">
-                <button type="submit" className="w-full bg-cyan-600 text-white py-2 rounded hover:bg-cyan-700 transition duration-200">
+                <button
+                  type="submit"
+                  className="w-full bg-cyan-600 text-white py-2 rounded hover:bg-cyan-700 transition duration-200"
+                >
                   Registrarse
                 </button>
               </div>
               <div className="text-center mt-4">
                 <label>
-                  ¿Ya tienes cuenta? <a href="/login" className="text-cyan-600 hover:underline">Inicia sesión</a>
+                  ¿Ya tienes cuenta?{" "}
+                  <a href="/login" className="text-cyan-600 hover:underline">
+                    Inicia sesión
+                  </a>
                 </label>
               </div>
             </form>
